47dc1c1eaa72a3ec7e31e53a12a906ccf2238634
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-02-13 05:01:25 -0300

    Adição método para adicionar itens

--- SearchSystem/SearchMode.java
+++ SearchSystem/SearchMode.java
@@ -1,4 +1,14 @@
 package SearchSystem;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class SearchMode {
+ private ArrayList <String> list = new ArrayList<>();
+ private String itemAdd;
+ 
+ private void addItem(){
+     itemAdd = JOptionPane.showInputDialog(null,"Digite um nome: ");
+     list.add(itemAdd);
+ }
 }
